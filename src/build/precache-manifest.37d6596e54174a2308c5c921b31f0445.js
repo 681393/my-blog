3f0f375561a60abd2a7a7f1b4ac7ce6582d8a889
Himanshu Jain <jain.himanshu1990@gmail.com>
2020-10-17 12:50:25 +0530

    First commit

---
+++ src/build/precache-manifest.37d6596e54174a2308c5c921b31f0445.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a003379ab7c3af917c1c4ec08fc38cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef71ef52883932e8647f",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "72cb77b74127f99ff4b1",
+    "url": "/static/js/2.f0c1cfc6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.f0c1cfc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef71ef52883932e8647f",
+    "url": "/static/js/main.64f7a5fd.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
